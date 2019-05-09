991144bbcb9fac0afed1b4b37ed69a113207be62
Ryan Sprott <rds2184@columbia.edu>
2019-05-09 11:35:53 -0400

    Add dev middleware

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
 })();
 
+app.use('/dist', express.static(path.resolve(__dirname, './dist')))
+
+require('./build/dev-server')(app)
+
 app.get('*', (request, response) => {
   response.write(indexHTML)
   response.end()
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 const app = new Vue({
   data: {
-    hello: 'hello world'
+    hello: 'hello world 2'
   }
 })
 
