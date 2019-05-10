82a9ca7e73f67cb229dfd1b38bc42ae41c910a39
Ryan Sprott <rdsprott@gmail.com>
2019-05-09 21:03:46 -0400

    Add style.

--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -4,7 +4,10 @@
         <source srcset="../images/leaflink.svg" media="(min-width: 800px)">
         <img id="logo" height="75px" src="../images/ll.svg" alt="LeafLink logo">
     </picture>
-    <h2>Hello {{hello}}</h2>
+    <h1>Hello {{hello}}</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
   </div>
 </template>
 
@@ -19,8 +22,14 @@ export default {
 </script>
 
 <style lang="scss">
-  h2 {
-    color: $blue-dream;
-    font-family: 'Poppins'
+  h1 {
+    color: $grandaddy-purp;
+    font-family: 'Poppins';
+    text-align: center;
+  }
+  p {
+    background-color: $white-widow;
+    color: $la-confidential;
+    font-family: 'Noto Sans';
   }
 </style>
