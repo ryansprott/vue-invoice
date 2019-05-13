00725d3f75df847112b54f5ef9b49c8793d1abeb
Ryan Sprott <rds2184@columbia.edu>
2019-05-13 15:16:47 -0400

    Add component.

---
+++ src/vue/components/invoice/InvoiceInputButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <a href="#"
+    class="btn btn-danger"
+    @click="deleteThisRow">
+      Delete
+  </a>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  props: ['row'],
+  methods: {
+    ...mapMutations(['deleteRow']),
+    deleteThisRow: function () {
+      this.deleteRow(this.row)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.btn-danger {
+  background-color: $grandaddy-purp;
+  border-color: $grandaddy-purp;
+  color: scale-color($green-crack, $lightness: -20%);
+}
+
+.btn-danger:hover {
+  background-color: $grandaddy-purp;
+  border-color: $grandaddy-purp;
+  color: $green-crack;
+}
+</style>
--- src/vue/components/invoice/InvoiceRow.vue
+++ src/vue/components/invoice/InvoiceRow.vue
@@ -10,11 +10,7 @@
       <invoice-input-rate :row="row" />
     </td>
     <td>
-      <a href="#"
-        class="btn btn-danger"
-        @click="deleteThisRow">
-          Delete
-      </a>
+      <invoice-input-button :row="row" />
     </td>
     <td class="text-right">
       <div>{{ getTotal }}</div>
@@ -26,7 +22,7 @@
 import InvoiceInputDescription from './InvoiceInputDescription.vue'
 import InvoiceInputQuantity from './InvoiceInputQuantity.vue'
 import InvoiceInputRate from './InvoiceInputRate.vue'
-import { mapMutations } from 'vuex'
+import InvoiceInputButton from './InvoiceInputButton.vue'
 
 export default {
   props: ['row'],
@@ -34,31 +30,12 @@ export default {
     'invoice-input-description': InvoiceInputDescription,
     'invoice-input-quantity': InvoiceInputQuantity,
     'invoice-input-rate': InvoiceInputRate,
+    'invoice-input-button': InvoiceInputButton
   },
   computed: {
     getTotal: function () {
       return (this.row.rate * this.row.quantity).toFixed(2)
     },
   },
-  methods: {
-    ...mapMutations(['deleteRow']),
-    deleteThisRow: function () {
-      this.deleteRow(this.row)
-    },
-  },
 }
 </script>
-
-<style lang="scss" scoped>
-.btn-danger {
-  background-color: $grandaddy-purp;
-  border-color: $grandaddy-purp;
-  color: scale-color($green-crack, $lightness: -20%);
-}
-
-.btn-danger:hover {
-  background-color: $grandaddy-purp;
-  border-color: $grandaddy-purp;
-  color: $green-crack;
-}
-</style>
