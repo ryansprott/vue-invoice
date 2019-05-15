caed471d1b1e936a0bd638393370ec4d3d84333c
Ryan Sprott <rds2184@columbia.edu>
2019-05-15 15:09:11 -0400

    Style table.

--- src/vue/components/invoice/InvoiceWrapper.vue
+++ src/vue/components/invoice/InvoiceWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="table-responsive" v-show="getNumRows > 0">
+  <div class="container bg-light table-responsive" v-show="getNumRows > 0">
     <table class="table">
       <invoice-header-row />
       <tbody v-for="(row, index) in getRows" :key="index">
